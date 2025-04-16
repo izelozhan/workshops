a232d20649023f8d18783ca1a96edd144f782438
Izel Ozhan <izelozhan@gmail.com>
2025-04-15 23:12:13 -0500

    calculator-1 mortgage calculator

---
+++ financial-calculators/src/main/java/FinancialCalculators.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class FinancialCalculators {
+    public static void main(String[] args) {
+        mortgageCalculator();
+
+    }
+
+    public static void mortgageCalculator() {
+        Scanner myScanner = new Scanner(System.in);
+        StringBuilder builder = new StringBuilder();
+//
+        double principal, annualInterestRate, loanTermYear, monthlyInterestRate, numOfMonthlyPayment;
+
+        System.out.println("What is your loan amount?");
+        principal = myScanner.nextDouble();
+        myScanner.nextLine();
+
+        System.out.println("What is your interest rate percentage?");
+        annualInterestRate = myScanner.nextDouble();
+        myScanner.nextLine();
+
+        System.out.println("What is your loan length (years)?");
+        loanTermYear = myScanner.nextDouble();
+
+        monthlyInterestRate = annualInterestRate / 12 / 100;
+        numOfMonthlyPayment = loanTermYear * 12;
+
+        //M=P×(i*(1+i)^n / ((1+i)^n)-1)
+        
+        double term1 = (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numOfMonthlyPayment));
+        double term2 = (Math.pow(1 + monthlyInterestRate, numOfMonthlyPayment) - 1);
+        double monthlyPayment = principal * (term1 / term2);
+
+        double totalInterest = (monthlyPayment * numOfMonthlyPayment) - principal;
+
+        builder.append("A $")
+                .append(principal)
+                .append(" loan at ")
+                .append(annualInterestRate)
+                .append("% interest for ")
+                .append(loanTermYear)
+                .append(" years would have a $")
+                .append(String.format("%.2f", monthlyPayment))
+                .append("/mo payment with a total interest of $")
+                .append(String.format("%.2f", totalInterest))
+                .append(".\nWould you like to apply for a loan today?");
+
+        String myMortgage = builder.toString();
+        System.out.println(myMortgage);
+    }
+}
