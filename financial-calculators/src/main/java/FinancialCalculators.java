719d5e63ae1877d291d8d87ebb7ecaa9c10afbee
Izel Ozhan <izelozhan@gmail.com>
2025-04-15 23:34:12 -0500

    calculator-2 future-value

--- financial-calculators/src/main/java/FinancialCalculators.java
+++ financial-calculators/src/main/java/FinancialCalculators.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class FinancialCalculators {
     public static void main(String[] args) {
-        mortgageCalculator();
-
+//        mortgageCalculator();
+        futureValue();
     }
 
     public static void mortgageCalculator() {
@@ -27,7 +27,7 @@ public class FinancialCalculators {
         numOfMonthlyPayment = loanTermYear * 12;
 
         //M=P×(i*(1+i)^n / ((1+i)^n)-1)
-        
+
         double term1 = (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numOfMonthlyPayment));
         double term2 = (Math.pow(1 + monthlyInterestRate, numOfMonthlyPayment) - 1);
         double monthlyPayment = principal * (term1 / term2);
@@ -48,5 +48,34 @@ public class FinancialCalculators {
 
         String myMortgage = builder.toString();
         System.out.println(myMortgage);
+    }
+
+    public static void futureValue() {
+        Scanner myScanner = new Scanner(System.in);
+        StringBuilder builder = new StringBuilder();
+
+        double principal, annualInterestRate, depositTermYear;
+
+        System.out.println("What is your deposit amount?");
+        principal = myScanner.nextDouble();
+        myScanner.nextLine();
+
+        System.out.println("What is your interest rate percentage?");
+        annualInterestRate = myScanner.nextDouble() / 100; //%
+        myScanner.nextLine();
+
+        System.out.println("What is your deposit length (years)?");
+        depositTermYear = myScanner.nextDouble();
+
+        double futureValue = principal * Math.pow(1 + (annualInterestRate / 365), (depositTermYear * 365));
+        double totalInterest = futureValue - principal;
+
+        builder.append("Your future value is $")
+                .append(String.format("%.2f", futureValue))
+                .append(" and the total interest earned $")
+                .append(String.format("%.2f", totalInterest));
+
+        String myDeposit = builder.toString();
+        System.out.println(myDeposit);
     }
 }
