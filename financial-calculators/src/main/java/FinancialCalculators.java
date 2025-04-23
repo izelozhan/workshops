f05c69e4869c143f154cbe9b18edaf61a57ed807
Izel Ozhan <izelozhan@gmail.com>
2025-04-22 22:33:58 -0500

    initial

--- financial-calculators/src/main/java/FinancialCalculators.java
+++ financial-calculators/src/main/java/FinancialCalculators.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class FinancialCalculators {
     public static void main(String[] args) {
-//        mortgageCalculator();
+        mortgageCalculator();
         futureValue();
     }
 
